e9300d74146facf97c9fa7737155c14465ba08b3
Gregory Petropoulos <gregpetrop@gmail.com>
2014-03-12 00:36:05 -0600

    cleaned up unfinished class

--- google_search.py
+++ google_search.py
@@ -4,13 +4,6 @@ from BeautifulSoup import BeautifulSoup
 import urllib, urllib2
 import re
 
-class record:
-  name        = ""
-  instutution = []
-  rank        = []
-  start       = []
-  end         = []
-
 def google_scrape(query):
     address = "http://www.google.com/search?q=%s&num=100&hl=en&start=0" % (urllib.quote_plus(query))
     request = urllib2.Request(address, None, {'User-Agent':'Mosilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11'})
