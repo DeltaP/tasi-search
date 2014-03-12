dac45a6e93ef718ac0cfdcc53ed495b6610ee916
Gregory Petropoulos <gregpetrop@gmail.com>
2014-03-12 14:35:56 -0600

    another cosmetic change

--- inspire_pub_search.py
+++ inspire_pub_search.py
@@ -2,12 +2,12 @@
 from bs4 import BeautifulSoup
 import urllib, urllib2
 
-paper_return = 5        #max number of papers to return from search (arbitrary)
+n_papers = 5        #max number of papers to return from search (arbitrary)
 
 def scrape_pub(query):
   address   = "http://inspirehep.net/search?ln=en&ln=en&p=find+author%3A%22"+(urllib.quote_plus(query))+"%22&of=hb&action_search=Search&sf=&so=d&rm=&rg=25&sc=0"
   request   = urllib2.Request(address, None, {'User-Agent':'Mosilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11'})
-  urlfile = urllib2.urlopen(request)
+  urlfile   = urllib2.urlopen(request)
   page      = urlfile.read()
   soup      = BeautifulSoup(page)
   pub_list  = soup.findAll('a', href=True, text='Detailed record')
@@ -24,7 +24,7 @@ def pub_record(pub_link):
 
   title       = soup.findAll(attrs={"name":"citation_title"})
   author_list = soup.findAll(attrs={"name":"citation_author"})
-  authors = [x['content'].encode('utf-8') for x in author_list]
+  authors     = [x['content'].encode('utf-8') for x in author_list]
   date        = soup.findAll(attrs={"name":"citation_publication_date"})
 
   if (len(title)>0):
@@ -41,6 +41,6 @@ if __name__ == '__main__':
     print "Searching:  " + name.rstrip()
     print"---------------------------------"
     pub_links = scrape_pub(name)
-    for li in pub_links[0:paper_return]:
+    for li in pub_links[0:n_papers]:
       pub_record(li)
     print "*\t*\t*\t*\t*\n"
